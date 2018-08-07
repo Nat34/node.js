f82d1d80afe889ccab198e734eeaf42e21029679
Nat34 <nathan.a.thomas@gmail.com>
2018-08-06 21:35:11 -0500

    move methods and variables to a module

--- app.js
+++ app.js
@@ -1,55 +1,15 @@
-//function to print message to console
-function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-    console.log(message);
-}
+/*
+ * Require profile module
+ */
+const profile = require('./profile.js');
 
-//Require https module
-const https = require('https');
-
-//Require http module
-const http = require('http');
-
-function printError(error) {
-    console.log(error.message);
-}
-
-function getProfile(username) {
-    try {
-        const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
-            if (response.statusCode == 200) {
-                let body ='';
-                //read the data
-                response.on('data', data => {
-                    body += data.toString();
-                });
-                response.on('end', () => {
-                    try {
-                        //parse the data
-                        const profile = JSON.parse(body);
-                        //print the data
-                        printMessage(
-                            username, 
-                            profile.badges.length, 
-                            profile.points.JavaScript
-                        );
-                    } catch(error) {
-                        printError(error);
-                    }
-                }); 
-            } else {
-                const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
-                const statusCode = new Error(message);
-                printError(statusCode);
-            }
-        });
-        request.on('error', printError);
-    } catch (error) {
-        printError(error);   
-    }
-
-}
-
-//capture command line args with process global object
+/*
+ * Capture command line args with process global object
+ */
 const users = process.argv.slice(2);
-users.forEach(getProfile);
+
+/*
+ *
+ * Execute the get method for each elem in user array
+ */
+users.forEach(profile.get);
---
+++ profile.js
@@ -0,0 +1,81 @@
+/*
+ * Require https module
+ */
+const https = require('https');
+
+/*
+ * Require http module
+ */
+const http = require('http');
+
+/*
+ * Prints message to the console
+ *
+ * @param string username    name of user
+ * @param int    badgecount  number of earned badges
+ * @param int    points      number of JavaScript points
+ *
+ */
+function printMessage(username, badgeCount, points) {
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+    console.log(message);
+}
+
+/*
+ * Prints error message to the console
+ *
+ * @param Error error instance of error object
+ *
+ */
+function printError(error) {
+    console.log(error.message);
+}
+
+/*
+ * Connects to an API and retreives information
+ *
+ * @param string username user's name gathered using the Process object
+ *
+ */
+function get(username) {
+    try {
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
+            if (response.statusCode == 200) {
+                let body ='';
+                //read the data
+                response.on('data', data => {
+                    body += data.toString();
+                });
+                response.on('end', () => {
+                    try {
+                        //parse the data
+                        const profile = JSON.parse(body);
+                        //print the data
+                        printMessage(
+                            username, 
+                            profile.badges.length, 
+                            profile.points.JavaScript
+                        );
+                    } catch(error) {
+                        printError(error);
+                    }
+                }); 
+            } else {
+                const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+                const statusCode = new Error(message);
+                printError(statusCode);
+            }
+        });
+        request.on('error', printError);
+    } catch (error) {
+        printError(error);   
+    }
+
+}
+
+/*
+ * Explicitly state the method available 
+ *  to those who require 'profile'
+ *
+ */
+module.exports.get = get;
